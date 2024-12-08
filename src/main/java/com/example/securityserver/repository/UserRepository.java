723156498ca9a3e9077786a654d6bc418af41ea0
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-12-08 21:49:52 +0900

    feat[1]: DB 연결 (JPA)

---
+++ src/main/java/com/example/securityserver/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.securityserver.repository;
+
+import com.example.securityserver.domain.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
